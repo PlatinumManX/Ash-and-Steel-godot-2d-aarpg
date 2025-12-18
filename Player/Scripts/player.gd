685e128aab87ceb418d8f4aed643ad983b09abb7
PlatinumManX <goldenahmed14@gmail.com>
2025-12-18 18:52:51 +0530

    Added Basic World, Camera and Movement Polish
    - Created an initial TileMap-based world with grass, hills, and path terrain
    - Added and refined collision for map structures (e.g., hill boundaries)
    - Implemented a Camera2D to follow the player during exploration
    - Fixed diagonal movement speed by normalizing the movement direction vector
    - Updated player motion_mode to Floating for smoother collision response and sliding

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -16,8 +16,13 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	#direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	#direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	#direction = direction.normalized() #normalized prevents Vector greater than 1. This to prevent higher diagional movement speed.
+	direction = Vector2(
+		Input.get_axis("left", "right"),
+		Input.get_axis("up", "down")
+	).normalized()
 	pass
 
 
