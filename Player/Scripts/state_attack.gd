0a7c1db3ba7cb9b5efd11bae00f71107c8736429
PlatinumManX <goldenahmed14@gmail.com>
2025-12-27 11:56:18 +0530

    Added camera bounds system using level and tilemap data
    - Added a dedicated player camera script for centralized camera control
    - Created a LevelManager autoload to manage level-wide data
    - Added a TileMap script to expose world boundaries
    - Integrated camera limits to prevent the camera from moving outside the playable area

--- Player/Scripts/state_attack.gd
+++ Player/Scripts/state_attack.gd
@@ -8,7 +8,7 @@ var attacking : bool = false
 @onready var idle: State = $"../Idle"
 @onready var walk: State = $"../Walk"
 @onready var audio: AudioStreamPlayer2D = $"../../Audio/AudioStreamPlayer2D"
-@onready var hurt_box: HurtBox = $"../../Interactions/HurtBox"
+@onready var hurt_box: HurtBox = %AttackHurtBox
 
 @onready var animation_player: AnimationPlayer = $"../../AnimationPlayer"
 @onready var attack_anim: AnimationPlayer = $"../../Sprite2D/AttackEffectSprite/AnimationPlayer"
