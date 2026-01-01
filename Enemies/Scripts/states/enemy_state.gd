a3b307f713bf5c50c6572c1739353e28676a1c61
PlatinumManX <goldenahmed14@gmail.com>
2026-01-01 17:57:06 +0530

    Added basic slime enemy with FSM-driven idle and walk behavior
    - Created a Slime enemy scene with idle and walk animations
    - Implemented an enemy finite state machine
    - Added idle and walk enemy states with randomized movement logic
    - Integrated smooth animation transitions
    - Introduced a global player manager autoload for future player–enemy interactions

---
+++ Enemies/Scripts/states/enemy_state.gd
@@ -0,0 +1,29 @@
+class_name EnemyState extends Node
+
+
+##Stores a reference to the enemy that this state belongs to
+var enemy : Enemy
+var state_machine : EnemyStateMachine
+
+
+##What happens when we initialize this state?
+func init() -> void:
+	pass
+
+
+##What happens when the enemy enters this state?
+func enter() -> void:
+	pass
+
+
+##What happens when the enemy exits this State?
+func exit() -> void:
+	pass
+
+
+func process( _delta : float ) -> EnemyState:
+	return null
+
+
+func physics( _delta : float ) -> EnemyState:
+	return null
